6e2d3d0aab63567875fc590b0011abf80733cfce
Anthony Salemo <ajssalemo@gmail.com>
2021-10-05 16:41:31 -0400

    able to fetch data from unsplash

--- app/views.py
+++ app/views.py
@@ -1,11 +1,17 @@
 import logging
 
+import requests
 from django.http import HttpResponse
 from django.shortcuts import render
+from unsplash.env import (UNSPLASH_API_ACCESS_KEY, UNSPLASH_API_PREFIX,
+                          UNSPLASH_API_SECRET_KEY)
+
 
 def index(request):
-    title='Unsplash'
-    return render(request, 'app/index.html', {'title': title})
+    search_unsplash_photos = requests.get(
+        f'{UNSPLASH_API_PREFIX}/search/photos?query="new york city"', headers={'Authorization': f'Client-ID {UNSPLASH_API_ACCESS_KEY}'})
+    print(search_unsplash_photos.text)
+    return render(request, 'app/index.html')
 
 
 def health(request):
