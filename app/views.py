8ecbf4298d791f3a1b9e5ba509b7a2f3d4903859
Anthony Salemo <ajssalemo@gmail.com>
2021-10-05 18:10:17 -0400

    able to display images

--- app/views.py
+++ app/views.py
@@ -3,15 +3,15 @@ import logging
 import requests
 from django.http import HttpResponse
 from django.shortcuts import render
-from unsplash.env import (UNSPLASH_API_ACCESS_KEY, UNSPLASH_API_PREFIX,
-                          UNSPLASH_API_SECRET_KEY)
+from unsplash.env import UNSPLASH_API_ACCESS_KEY, UNSPLASH_API_PREFIX
 
 
 def index(request):
     search_unsplash_photos = requests.get(
         f'{UNSPLASH_API_PREFIX}/search/photos?query="new york city"', headers={'Authorization': f'Client-ID {UNSPLASH_API_ACCESS_KEY}'})
-    print(search_unsplash_photos.text)
-    return render(request, 'app/index.html')
+    search_photos_json = search_unsplash_photos.json()
+    print(search_photos_json['results'])
+    return render(request, 'app/index.html', {'search_photos_json': search_photos_json['results']})
 
 
 def health(request):
