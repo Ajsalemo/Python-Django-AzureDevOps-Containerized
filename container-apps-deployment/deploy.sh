4b8bce27bad208cf09d6909c8f2248850ffeeaa5
Anthony Salemo <ajssalemo@gmail.com>
2021-11-22 14:46:31 -0500

    changed the ARM template to be more flexible with input

--- container-apps-deployment/deploy.sh
+++ container-apps-deployment/deploy.sh
@@ -6,4 +6,16 @@ az deployment group create \
   --parameters \
       environment_name="$CONTAINERAPPS_ENVIRONMENT" \
       location="$LOCATION" \
-      name="$NAME"
+      name="$NAME" \
+      acr-password="$ACR_PASSWORD" \
+      postgres-engine-secret-ref="$POSTGRES_ENGINE_SECRET_REF" \
+      postgres-name-secret-ref="$POSTGRES_NAME_SECRET_REF" \
+      postgres-user-secret-ref="$POSTGRES_USER_SECRET_REF" \
+      postgres-host-secret-ref="$POSTGRES_HOST_SECRET_REF" \
+      postgres-port-secret-ref="$POSTGRES_PORT_SECRET_REF" \
+      postgres-password-secret-ref="$POSTGRES_PASSWORD_SECRET_REF" \
+      unsplash-api-prefix-secret-ref="$UNSPLASH_API_PREFIX_SECRET_REF" \
+      unsplash-api-access-key-secret-ref="$UNSPLASH_API_ACCESS_KEY_SECRET_REF" \
+      unsplash-api-secret-key-secret-ref="$UNSPLASH_API_SECRET_KEY_SECRET_REF"
+
+
